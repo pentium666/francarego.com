30a76176c3755209c96c9bd3cfdb99f01084a60d
Maximilian Carr <maximilianacarr@gmail.com>
2016-12-04 22:11:18 -0500

    added favicon, optimized scripts

--- public/js/scripts.php
+++ public/js/scripts.php
@@ -1,6 +1,18 @@
+<!-- jquery -->
+<script
+	src="https://code.jquery.com/jquery-3.1.1.min.js"
+integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+crossorigin="anonymous"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+crossorigin="anonymous">
+</script>
+
 <script type="text/javascript">
     window.onload = function() {
-    	if ($(window).height() > $("html").height()) {
+    	if ($(window).height() > $("html").height()) { //sets footer position
     		document.getElementById("footer").style.position = "absolute";
     		document.getElementById("footer").style.bottom = 0;
     	}
--- views/header.php
+++ views/header.php
@@ -18,18 +18,13 @@ To this end, Communists of various nationalities have assembled in London and sk
 following manifesto, to be published in the English, French, German, Italian, Flemish and Danish
 languages.
 -->
+		<link rel="shortcut icon" href ="favicon.ico">
 
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet"
 		href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 		integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
 		crossorigin="anonymous">
-
-		<!-- jquery -->
-		<script
-  		src="https://code.jquery.com/jquery-3.1.1.min.js"
-		integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-		crossorigin="anonymous"></script>
 
 		<!-- Optional theme -->
 		<link rel="stylesheet"
@@ -44,9 +39,3 @@ languages.
 		<link rel="stylesheet"
 		href="/css/main.css"
 		type="text/css">
-
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-		integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-		crossorigin="anonymous">
-		</script>
