c2995125dc4ee0c512bbb0cd75460bd7e73c60b0
Maximilian Carr <maximilianacarr@gmail.com>
2016-12-08 14:03:54 -0500

    Random chance of dank

--- public/index.php
+++ public/index.php
@@ -5,6 +5,9 @@ require("../config/config.php");
 $json = file_get_contents("data/images.json"); //read links to images
 $rows = json_decode($json, true); //decode JSON to array
 
-render("home.php", ["title" => "French King Photography", "rows" => $rows["images"]]);	
+if(rand(1, 420) == 420)
+	render("dank.php");
+else
+	render("home.php", ["title" => "French King Photography", "rows" => $rows["images"]]);
 
 ?>
---
+++ views/dank.php
@@ -0,0 +1,1 @@
+<iframe width=100% height=100% src="https://www.youtube.com/embed/oNVfrxkHj1M" frameborder="0" allowfullscreen></iframe>
