91323c870357aa6eaacd97da757d6a8097dc304b
Maximilian Carr <maximilianacarr@gmail.com>
2016-12-15 22:27:37 -0500

    Finally fixed background image! This took two hours to find one fucking css attribute that was the problem. I wasted an entire evening that I should have spent doing actual schoolwork. Oh well.

--- views/home.php
+++ views/home.php
@@ -2,8 +2,13 @@
 	<h1 class="site-title"> Welcome to French King Photography</h1>
 </div>
 <script>
-	document.getElementsByTagName("html")[0].style = "height: 100%";
-	document.getElementsByTagName("body")[0].style = "height: 100%";
-	document.getElementsByTagName("html")[0].id = "homepage";
-	document.getElementsByTagName("body")[0].id = "homepage";
+	document.getElementsByTagName("html")[0].style = "height: 100%; min-height: 100%;";
+	document.getElementsByTagName("body")[0].style = "min-height: 100%";
+	document.getElementsByTagName("html")[0].className = "homepage";
+	document.getElementsByTagName("body")[0].className = "homepage";
+
+	//if on mobile
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+
+	}
 </script>
